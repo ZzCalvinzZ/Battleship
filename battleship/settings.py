02e670c95f9624f43c5c0c13d74aaf1a401c6c73
calvin <calvinkcollins@gmail.com>
2015-02-15 09:39:08 -0500

    heroku

--- battleship/settings.py
+++ battleship/settings.py
@@ -2,6 +2,15 @@
 # Static asset configuration
 import os
 
+# here() gives us file paths from the root of the system to the directory
+# holding the current file.
+here = lambda * x: os.path.join(os.path.abspath(os.path.dirname(__file__)), *x)
+
+PROJECT_ROOT = here("..")
+# root() gives us file paths from the root of the system to whatever
+# folder(s) we pass it starting at the parent directory of the current file.
+root = lambda * x: os.path.join(os.path.abspath(PROJECT_ROOT), *x)
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
@@ -125,8 +134,7 @@ TEMPLATE_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     '/home/calvin/dev/battleship/templates/',
-    os.path.join(BASE_DIR, "templates"),
-    # root('templates'),
+    root('templates'),
 )
 
 INSTALLED_APPS = (
