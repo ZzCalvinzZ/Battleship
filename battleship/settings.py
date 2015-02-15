74670127d50b74de764845dc25fbf4f57724c6ed
calvin <calvinkcollins@gmail.com>
2015-02-14 19:16:23 -0500

    deployment stuff

--- battleship/settings.py
+++ battleship/settings.py
@@ -1,4 +1,20 @@
 # Django settings for battleship project.
+
+# here() gives us file paths from the root of the system to the directory
+# holding the current file.
+here = lambda * x: os.path.join(os.path.abspath(os.path.dirname(__file__)), *x)
+
+PROJECT_ROOT = here("..")
+# root() gives us file paths from the root of the system to whatever
+# folder(s) we pass it starting at the parent directory of the current file.
+root = lambda * x: os.path.join(os.path.abspath(PROJECT_ROOT), *x)
+
+....
+
+TEMPLATE_DIRS = (
+    root('templates'),
+)
+
 import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
